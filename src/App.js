59787f1bc9f1c09d9e50b1c74b782b9769f5b78f
Taylor Pechacek <taylortpechacek@gmail.com>
2020-01-11 11:41:20 +1100

    updated app to make sure tests pass

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there! Something new</p>
+        <p>I was changed on the Feature branch.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the Feature branch.
+          Learn React
         </a>
       </header>
     </div>
